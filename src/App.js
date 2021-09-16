9a1b0f1e544f68cbcfc69ac36efe45539664c7a4
babyazalea <usun12@hotmail.com>
2021-09-16 10:55:53 +0900

    avoid infinite loop with useCallback

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
-      );
+  // sendRequest func 안에는 setState method가 포함되어 있다.
+  // 아래의 useEffect에서 sendRequest(fetchTasks)를 불러오면, sendRequest func 내부의
+  // setState가 작동하고, 변경된 state는 App Component로 전달된다.
+  // 이때 변경된 state를 감지한 App Component는 자동으로 re-evluate(re-rendering)되어
+  // 다시 useEffect에서 사용된 sendRequest를 '새롭게' 불러오게 되고
+  // 다시 sendRequest func 내부의 setState가 작동하고,
+  // 변경된 state를 감지한 App Component가 re-rendering...
+  // 무한 루프(infinite loop)에 빠지게 된다!
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
+  useEffect(() => {
+    const transformTasks = (taskObj) => {
       const loadedTasks = [];
 
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
       }
 
       setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
+    };
 
-  useEffect(() => {
-    fetchTasks();
-  }, []);
+    fetchTasks(
+      {
+        url: "https://all-the-practice-default-rtdb.firebaseio.com/tasks.json",
+      },
+      transformTasks
+    );
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
